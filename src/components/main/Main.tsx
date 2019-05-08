c534187a8047b9dffa14b9e2331d2fdd7512975d
diegodsgarcia <diego.ds.garcia@gmail.com>
2019-05-08 13:00:59 -0300

    Fix animation dsg

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -33,15 +33,15 @@ class Main extends Component {
     this.scrollText.innerHTML = this.scrollText.textContent.trim().replace(/(\S+?)/g, '<span>$1</span>');
     const title = this.title.querySelectorAll('span');
     const scrollText = this.scrollText.querySelectorAll('span');
-    const cursor = new TimelineMax().to(this.cursor, 1, { autoAlpha: 0, repeat: -1, repeatDelay: 0.3, delay: -1, ease: new SteppedEase(1).config(1) })
 
     this.tl
       .staggerFromTo(title, .3, { display: 'none'}, { display: 'inline' }, .2)
-      .add(cursor)
+
       .staggerFromTo(scrollText, .3, { display: 'none'}, { display: 'inline' }, .08)
       .set(this.scrollIcon, { rotation: 45, x: '-50%' })
       .from(this.scrollIcon, .3, { y: 20, autoAlpha: 0 });
     this.tl.play();
+    TweenMax.to(this.cursor, 1, { autoAlpha: 0, repeat: -1, repeatDelay: 0.3, delay: -1, ease: new SteppedEase(1).config(1) })
   }
 
   scrollDown() {
